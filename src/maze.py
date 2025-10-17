23c17322e478bdb9e5613f5ae9100d231d9a32a0
Bob Dowling <bob.dowling.64@gmail.com>
2025-10-17 15:06:01 +0100

    Added Direction.

--- src/maze.py
+++ src/maze.py
@@ -9,3 +9,22 @@ class Item(pydantic.BaseModel):
     name: str = pydantic.Field(
         description="The name of the item as appearing in inventory lists.",
     )
+
+
+class Direction(pydantic.BaseModel):
+    """A direction out of a room in the maze."""
+
+    coordinates: tuple[int, ...] = pydantic.Field(
+        description="The values of the coordinates of the direction.",
+    )
+
+    def __neg__(self) -> "Direction":
+        """Directions are used as the labels for doors out of a room in the maze.
+        The assumption is that if direction D leads from room A to room B then direction -D will lead from
+        room B to room A.
+        """
+        xyz = tuple(-x for x in self.coordinates)
+        return Direction(coordinates=xyz)
+
+    def __hash__(self):
+        return self.coordinates.__hash__()
--- tst/testmaze.py
+++ tst/testmaze.py
@@ -22,5 +22,34 @@ class TestItem(unittest.TestCase):
             thing = maze.Item()  # type: ignore
 
 
+class TestDirection(unittest.TestCase):
+    def testCreation(self) -> None:
+        xy = (1, 0)
+        direction = maze.Direction(coordinates=xy)
+        self.assertEqual(direction.coordinates, xy)
+
+    def testNegation(self) -> None:
+        xy1 = (1, 0)
+        xy2 = (-1, 0)
+        direction1 = maze.Direction(coordinates=xy1)
+        direction2 = -direction1
+        self.assertEqual(direction2.coordinates, xy2)
+
+    def testHash0(self) -> None:
+        xy = (1, 0)
+        direction1 = maze.Direction(coordinates=xy)
+        direction2 = -direction1
+        direction3 = -direction2
+        self.assertNotEqual(id(direction1), id(direction3))
+        self.assertNotEqual(id(direction1.coordinates), id(direction3.coordinates))
+        self.assertEqual(hash(direction1), hash(direction3))
+
+    def testHash1(self) -> None:
+        xy = (1, 0)
+        direction1 = maze.Direction(coordinates=xy)
+        direction2 = -direction1
+        self.assertNotEqual(hash(direction1), hash(direction2))
+
+
 if __name__ == "__main__":
     unittest.main()
