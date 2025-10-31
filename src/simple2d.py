5fdd6d5d433d3b948d283f085b3c823cb32abcb3
Bob Dowling <bob.dowling.64@gmail.com>
2025-10-31 13:17:08 +0000

    Starting work on the simple 2D maze system, including SVG drawing.

---
+++ src/simple2d.py
@@ -0,0 +1,15 @@
+import maze
+import svg
+
+
+class SimpleMaze2D(maze.Maze):
+    def __init__(
+        self,
+        M: int,
+        N: int,
+    ) -> None:
+        """Create a M×N 2-D maze.
+        """
+        super().__init__(
+            limits = maze.Coordinates(coordinates=(M,N)),
+        )
---
+++ tst/testsimple2d.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import pydantic
+import random
+import sys
+import unittest
+
+f = Path(__file__)
+s = f.parent.parent / "src"
+sys.path.append(str(s))
+print(__file__)
+del f, s
+import maze  # type: ignore
+import simple2d  # type: ignore
+
+
+class TestSimple2D(unittest.TestCase):
+    def testCreation(self) -> None:
+        M,N = 5,6
+        thing = simple2d.SimpleMaze2D(M,N)
+        self.assertEqual((M,N), thing.limits.coordinates)
+        
+
+
+
+if __name__ == "__main__":
+    unittest.main()
